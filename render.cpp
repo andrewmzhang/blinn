88f4331ad86a508ca938ba5bde341b8954f65fd1
Andrew Zhang <me@andrewmzhang.com>
2017-06-20 15:25:33 -0700

    Added render.h

---
+++ render.cpp
@@ -0,0 +1,67 @@
+#include "color.h"
+#include <iostream>
+#include <string>
+#include "render.h"
+#include "dbg.h"
+
+using namespace std;
+
+
+render::render(const int h, const int w) {
+
+	this->height = h;
+	this->width = w;
+
+}
+
+
+void render::render_frame() const {
+
+	cout << "No render function available" << endl;
+
+	return;
+}
+
+
+int render::get_width() const {
+
+	return this->width;
+
+}
+
+int render::get_height() const {
+
+	return this->height;
+
+}
+
+void render::set_height(int h) {
+
+	this->height = h;
+
+}
+
+void render::set_width(int w) {
+
+	this->width = w;
+
+}
+
+
+void render::test() {
+
+	cout << "Test function" << endl;
+
+}
+
+
+const char* to_string() {
+
+
+	return "Hard coded string";
+
+}
+
+
+
+
--- render.h
+++ render.h
@@ -1,9 +1,31 @@
-#ifndef
+#ifndef __render_h__
+#define __render_h__
+
+#include "color.h"
+
 class render {
+
+
+private:
+	int width;
+	int height;
+	color **frame;
+	void clean();
 
 public:
 
-	void render(char** 
+	render(const int h = 0, const int w = 0);
 
+	void render_frame() const;
+	
+	int get_width() const;
+	int get_height() const;
 
+	void set_height(int h);
+	void set_width(int w);
+
+	static void test();
+	const char* to_string() const;
+
+};
 #endif
