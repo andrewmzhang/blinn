396725e29d544233305cf3dd5ad9000a609c75eb
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-05-27 09:39:36 -0700

    Added config command line arg

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,17 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
 
     debug("Debug Mode ON!");
 
-    config conf("C:\\Users\\andrewmzhang\\CLionProjects\\blinn\\src\\config.txt");
+    if (argc != 2) {
+        throw std::runtime_error("Wrong number of arguments");
+    }
+
+    config conf(argv[1]);
 
     tracer t(&conf);
-
     blobsys sys(&conf);
 
 
